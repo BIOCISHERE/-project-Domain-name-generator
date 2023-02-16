9e9be6d612126056e718c44d2fcd418294d008d9
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-02-16 16:28:30 +0000

    Generator done with comments

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+//----------Generator Start----------//
+    //--------Generator List Start--------//
 let pron =["The", "Our", "Tom's"];
 let adj = ["Peaceful", "Rapid", "Terrible"];
 let noun = ["Kilometer", "Station", "Machine"];
 let dom = [".com", ".tv", ".org"];
-
+    //--------Generator List End--------//
+        //------Generator Loop Start------// 
 for(let i = 0; i < pron.length; i++){
     for(let j = 0; j < adj.length; j++){
         for(let k = 0; k < noun.length; k++){
@@ -12,3 +15,5 @@ for(let i = 0; i < pron.length; i++){
         }
     }
 }
+        //------Generator Loop End------//
+//----------Generator End----------//
