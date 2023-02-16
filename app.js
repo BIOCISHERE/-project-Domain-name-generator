31c5c74a2981429232ede15fdae2720770ac1212
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-02-16 16:22:54 +0000

    generator done

---
+++ app.js
@@ -0,0 +1,14 @@
+let pron =["The", "Our", "Tom's"];
+let adj = ["Peaceful", "Rapid", "Terrible"];
+let noun = ["Kilometer", "Station", "Machine"];
+let dom = [".com", ".tv", ".org"];
+
+for(let i = 0; i < pron.length; i++){
+    for(let j = 0; j < adj.length; j++){
+        for(let k = 0; k < noun.length; k++){
+            for( let l = 0; l < dom.length; l++){
+                console.log(`${pron[i]}${adj[j]}${noun[k]}${dom[l]}`)
+            }
+        }
+    }
+}
